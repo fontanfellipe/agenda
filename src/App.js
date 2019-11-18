9d106674800833b99ccc32e3cae1d8c852dafe72
Fellipe Coelho <fontan.fellipe@gmail.com>
2019-11-17 23:41:14 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
+import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+
+class App extends React.Component{
+  state = {
+    
+  };
+
+  render(){
+    
+    const columnStyle = {
+      border: '1px dotted black'
+    }
+
+    return (
+      <React.Fragment>
+        <MDBRow>
+        <MDBCol style={columnStyle} md="9">
+          <Event time='10:00' title='Meeting with John' />
+          {/* <Event hour='12:00' title='Lunch with amorzinhos'/> */}
+        </MDBCol>
+        <MDBCol style={columnStyle} md="3"/>
+        </MDBRow>
+      </React.Fragment>
+    )
+  }
+}
+
+class Event extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      time: this.props.time,
+      title: this.props.title 
+  }
+ 
+  };
+  render(){
+
+    const handleChangeTitle = () => this.setState({title: "My new state title"}, console.log(this.state.title))
+    const handleChangeTime = () => this.setState({time: '11:00'})
+
+    return (
+      <React.Fragment>
+        <h3>{this.props.time} - {this.props.title}</h3>
+        <button onClick={handleChangeTitle}>Change title</button>
+        <button onClick={handleChangeTime}>Change time</button>
+
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
